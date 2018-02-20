5d3f2a7b5c1883c13742a5081ef656838fa98690
Erik Pukinskis <erik@snowedin.net>
2018-02-19 17:06:13 -0800

    Editor can quote a string

--- editor-test.js
+++ editor-test.js
@@ -8,8 +8,12 @@ runTest(
     var editor = new Editor()
 
     function expectSymbols(line, intro, outro) {
-      expect(editor.getIntroSymbols(line)).to.deep.equal(intro)
-      expect(editor.getOutroSymbols(line)).to.deep.equal(outro)
+      var introSymbols = editor.getIntroSymbols(line)
+      var outroSymbols = editor.getOutroSymbols(line)
+      var message = "expected intro symbols "+JSON.stringify(introSymbols)+" on line "+line+" but editor thinks they are "+JSON.stringify(intro)
+      expect(introSymbols).to.deep.equal(intro, message)
+      var otherMessage = "expected outro symbols "+JSON.stringify(outroSymbols)+" to deep equal "+JSON.stringify(outro)
+      expect(outroSymbols).to.deep.equal(outro, otherMessage)
     }
 
     function expectText(line, text) {
@@ -18,36 +22,78 @@ runTest(
     function expectCursor(line, column) {
     }
 
-    editor.type("a")
+    editor.text(0, "a")
     expectSymbols(0, ["quote"], ["quote"])
+    done.ish("string gets quoted!")
     expectText(0, "a")
+    done.ish("string gets identified out")
 
-    editor.type("p")
+    editor.text(0, "\"ap\"")
     expectSymbols(0, ["quote"], ["quote"])
+    done.ish("quotes are ok after editing string")
     expectText(0, "ap")
+    done.ish("string still separate")
 
-    editor.type("earedAWild(")
+    editor.text(0, "\"appearedAWild(\"")
     expectSymbols(0, [], ["left-paren"])
+    done.ish("function calls recognized")
     expectSymbols(1, [], ["right-paren"])
+    done.ish("next line gets function call closer")
     expectText(0, "appearedAWild")
+    done.ish("function name gets split out")
     expectCursor(1,0)
+    done.ish("cursor drops to first arg")
+    expectText(1, Editor.EMPTY)
+    done.ish("first arg is empty")
 
-    editor.type("browser-bridge")
+    editor.text(1, "b)")
+    throw new Exception("need more done.ish!")
+    expectSymbols(1, ["quote"], ["quote", "right-paren"])
+    expectText(1, "b")
+
+    editor.text(1, "\"browser-bridge\")")
     expectSymbols(1, ["quote"], ["quote", "right-paren"])
     expectText(1, "browser-bridge")
 
     editor.pressEnter()
     expectSymbols(1, ["quote"], ["quote", "comma"])
     expectSymbols(2, [], ["right-paren"])
-    expectCursor(2,0)
+    expectCursor(2,1)
+    expectText(2, Editor.EMPTY)
 
-    editor.type("f")
+    editor.text(2, "f)")
     expectSymbols(2, ["quote"], ["quote", "right-paren"])
     expectText(2, "f")
+    expectCursor(2, 1)
 
-    editor.type("function ")
+    editor.text(2, "\"function \")")
     expectSymbols(2, ["function"], ["arguments-open", "arguments-close", "curly-open"])
-    expectSymbols(3, ["curly-close", "right-paren"])
+    expectSymbols(3, [], ["curly-close", "right-paren"])
+    expectText(2, Editor.EMPTY)
+    expectCursor(2, 1)
+    expectText(3, Editor.EMPTY)
+
+    editor.text(2, "function s(){")
+    expectSymbols(2, ["function"], ["arguments-open", "arguments-close", "curly-open"])
+    expectText(2, "s")
+    expectCursor(2, 1)
+
+    editor.pressEnter()
+    expectText(3, Editor.EMPTY)
+    expectCursor(3, 1)
+
+    editor.text(3, "b})")
+    expectSymbols(3, ["quote"], ["quote", "curly-close", "right-paren"])
+    expectText(3, "b")
+
+    editor.text(3, "\"b(\"})")
+    expectSymbols(3, [], ["left-paren"])
+    expectText(3, "b")
+    expectText(4, Editor.EMPTY)
+    expectSymbols(4, [], ["right-paren", "curly-close", "right-paren"])
+
+    editor.text(4, "hi)})")
+    expectSymbols(4, ["quote"], ["quote", "right-paren", "curly-close", "right-paren"])
 
     done()
   }
--- editor.js
+++ editor.js
@@ -1,16 +1,52 @@
 module.exports = Editor
 
 function Editor() {
+  this.currentLine = 0
+  this.intros = {}
+  this.outros = {}
+  this.closing = {}
 }
 
-Editor.prototype.type = function(text) {
+var QUOTE = "\""
+
+Editor.prototype.text = function(lineNumber, text) {
+  var expression = lineToExpression(text)
+
+  debugger
+  if (expression.kind == "string literal") {
+    this.intros[lineNumber] = "quote"
+    this.outros[lineNumber] = "quote"
+  }
+}
+
+function lineToExpression(text) {
+  return {
+    kind: "string literal"
+  }
 }
 
 Editor.prototype.pressEnter = function() {
 } 
 
-Editor.prototype.getIntroSymbols = function() {
+Editor.prototype.getIntroSymbols = function(lineNumber) {
+  debugger
+  var symbol = this.intros[lineNumber]
+  if (symbol) {
+    return [symbol]
+  } else {
+    return []
+  }
 } 
 
-Editor.prototype.getOutroSymbols = function() {
+Editor.prototype.getOutroSymbols = function(lineNumber) {
+  var outro = this.outros[lineNumber]
+  var closing = this.closing[lineNumber]
+  var symbols = []
+  if (outro) {
+    symbols.push(outro)
+  }
+  if (closing) {
+    symbols = symbols.concat(closing)
+  }
+  return symbols
 } 
