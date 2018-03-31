ddf42cbb1631054d1f944d013537805c1c46361a
Erik Pukinskis <erik@snowedin.net>
2018-03-31 11:47:07 -0700

    Three target hello-world programs for usability testing

---
+++ hello-world.js
@@ -0,0 +1,36 @@
+
+codeLibrary.add(
+  "hello world",
+  ["appeared-a-wild", "web-element"],
+  function(appearedAWild, webElement) {
+    appearedAWild(
+      "browser-bridge",
+      function(bridge) {
+        bridge.send(
+          webElement(
+          "hello, world"))})
+
+
+library.using(
+  ["issueBond", "accountability"],
+  function(issueBond, accountability) {
+    issueBond.lineItem (
+      "workshop" ,
+      "jan-7-2108@bobby" ,
+      "build roof, 6 hours" ,
+      6 * 1500 )
+    accountability()})
+
+
+library.define(
+  "a-panel-bond",
+  ["issue-bond", "inches", "sell-bond"],
+  function(issueBond, inches, sellBond) {
+    issueBond("a-panel", "Wall panel A", "Erik Pukinskis")
+    var wallHeight = 88
+    issueBond.tasks("a-panel", [
+      "reserve a truck",
+      "buy materials",
+      "cut 4 steel studs to "+inches(
+        wallHeight)])
+    sellBond("a-panel")})
